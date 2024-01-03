c2f279635e0c539905ef27cc331d9ab2cf15a8e6
Suchet Bhalla <bhalla.suchet@gmail.com>
2024-01-03 23:32:10 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,30 +1,28 @@
-#If there are no PDFs, then the program exits
-def check(A, M):
-    import sys
-
-    if ( not(A) ):
-        print(M)
-        sys.exit()
-
-#This function merges the files
+"""
+1. This function merges multiple PDF files into 1 file.
+2. The first argument is a list of file-names.
+"""
 def merger(files, name= 'new'):
 
-    #Error check
-    num= len(files)
-    check(num > 0, "Zero files in list" )
-
-
+    from sys import exit
     from PyPDF2 import PdfMerger
+    
+    #Error check
+    num = len(files)
+    if num == 0:
+        print("No PDF files in the current directory")
+        exit(0)
 
+    
     merger= PdfMerger()
-
+    
     for i in range(num):
         merger.append(files[i])
-
+    
     merger.write( name + '.pdf')
     merger.close()
 
-    del PdfMerger
+    del PdfMerger, sys
 
     print("---Files merged---")
 
@@ -32,6 +30,12 @@ def merger(files, name= 'new'):
 #Main
 import glob
 
+"""
+1. Identifies PDF files in the current directory
+2. Saves the filesnames in a list 
+3. Sorts the filenames alphabetically
+- To specify the order (of documents) in the merged PDF, append numerical prefixes to the file names. Rg. "1.pdf", "2.pdf", "3.pdf", etc.
+"""
 file_names = sorted(glob.glob(".\*.pdf"))
 
 merger(file_names, name = "Merged_Files")
